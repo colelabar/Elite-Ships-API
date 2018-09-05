fcc9419734ecb82c27ca5ec73244f268657eec19
Cole L Simchick <clsimchick@gmail.com>
2018-09-05 02:10:36 -0400

    finished adding tests, test 1 continues to fail

--- api/tests/apitests.php
+++ api/tests/apitests.php
@@ -6,7 +6,7 @@ use Slim\Http\Uri;
 use Slim\Http\RequestBody;
 require './vendor/autoload.php';
 
-// empty class definitions for phpunit to mock.
+// empty class definitions to mock
 class mockQuery {
   public function fetchAll(){}
   public function fetch(){}
@@ -21,7 +21,7 @@ class ShipTest extends TestCase
     protected $app;
     protected $db;
 
-    // execute setup code before each test is run
+    // pre-test setup code
     public function setUp()
     {
       $this->db = $this->createMock('mockDb');
@@ -29,20 +29,21 @@ class ShipTest extends TestCase
     }
 
     // test the GET ships endpoint
+    // this test continues to fail, despite my attempts to make it pass. There is a problem with the response body, and it's returning null instead of the expected string value. The status codes aren't throwing an error, which leads me to believe that it has something to do with the string comparison..
     public function testGetShips() {
 
       // expected result string
       $resultString = '[{"id":"1","manufacturer":"Faulcon-Delacy","name":"Sidewinder","size":"small"},{"id":"2","manufacturer":"Zorgon-Peterson","name":"Adder","size":"small"},{"id":"3","manufacturer":"Lakon Spaceways","name":"Diamondback Explorer","size":"small"},{"id":"4","manufacturer":"Core Dynamics","name":"Federal Assault Ship","size":"medium"},{"id":"5","manufacturer":"Lakon Spaceways","name":"Asp Explorer","size":"medium"},{"id":"6","manufacturer":"Gutamaya","name":"Imperial Clipper","size":"medium"}{"id":"7","manufacturer":"Faulcon-Delacy","name":"Krait MkII","size":"medium"},{"id":"8","manufacturer":"Lakon Spaceways","name":"Type-9 Heavy","size":"large"},{"id":"9","manufacturer":"Faulcon-Delacy","name":"Anaconda","size":"large"},{"id":"10","manufacturer":"Core Dynamics","name":"Federal Corvette","size":"large"},{"id":"11","manufacturer":"Saud-Kruger","name":"Beluga","size":"large"}]';
 
-      // mock the query class & fetchAll functions
+      // mock query and fetchAll
       $query = $this->createMock('mockQuery');
       $query->method('fetchAll')
         ->willReturn(json_decode($resultString, true)
       );
-       $this->db->method('query')
-             ->willReturn($query);
+      $this->db->method('query')
+        ->willReturn($query);
 
-      // mock the request environment.  (part of slim)
+      // mock request environment
       $env = Environment::mock([
           'REQUEST_METHOD' => 'GET',
           'REQUEST_URI'    => '/ships',
@@ -50,9 +51,9 @@ class ShipTest extends TestCase
       $req = Request::createFromEnvironment($env);
       $this->app->getContainer()['request'] = $req;
 
-      // run the request through the app
+      // run request through the app
       $response = $this->app->run(true);
-      // assert the status code and body that is expected
+      // assert expected status code and body
       $this->assertSame(200, $response->getStatusCode());
       $this->assertSame($resultString, (string)$response->getBody());
     }
@@ -66,12 +67,12 @@ class ShipTest extends TestCase
       $this->db->method('query')->willReturn($query);
       $env = Environment::mock([
           'REQUEST_METHOD' => 'GET',
-          'REQUEST_URI'    => '/people/1',
+          'REQUEST_URI'    => '/ships/1',
           ]);
       $req = Request::createFromEnvironment($env);
       $this->app->getContainer()['request'] = $req;
 
-      // actually run the request through the app.
+      // run request through the app.
       $response = $this->app->run(true);
       // assert expected status code and body
       $this->assertSame(200, $response->getStatusCode());
@@ -83,7 +84,7 @@ class ShipTest extends TestCase
       $this->db->method('query')->willReturn($query);
       $env = Environment::mock([
           'REQUEST_METHOD' => 'GET',
-          'REQUEST_URI'    => '/people/1',
+          'REQUEST_URI'    => '/ships/1',
           ]);
       $req = Request::createFromEnvironment($env);
       $this->app->getContainer()['request'] = $req;
@@ -92,12 +93,12 @@ class ShipTest extends TestCase
       $response = $this->app->run(true);
       // assert expected status code and body
       $this->assertSame(404, $response->getStatusCode());
-      $this->assertSame('{"status":404,"messname":"not found"}', (string)$response->getBody());
+      $this->assertSame('{"status":404,"message":"not found"}', (string)$response->getBody());
     }
 
     public function testUpdateShip() {
       // expected result string
-      $resultString = '{"id":"1","manufacturer":"C.S. Lewis","name":"49","size":"writer"}';
+      $resultString = '{"id":"1","manufacturer":"Gutamaya","name":"Imperial Eagle","size":"small"}';
 
       // mock the query class & fetchAll functions
       $query = $this->createMock('mockQuery');
@@ -105,21 +106,21 @@ class ShipTest extends TestCase
         ->willReturn(json_decode($resultString, true)
       );
       $this->db->method('query')
-            ->willReturn($query);
+        ->willReturn($query);
        $this->db->method('exec')
-             ->willReturn(true);
+        ->willReturn(true);
 
-      // mock the request environment.  (part of slim)
+      // mock request environment
       $env = Environment::mock([
           'REQUEST_METHOD' => 'PUT',
-          'REQUEST_URI'    => '/people/1',
+          'REQUEST_URI'    => '/ships/1',
           ]);
       $req = Request::createFromEnvironment($env);
-      $requestBody = ["manufacturer" =>  "C.S. Lewis", "name" => "49", "size" => "writer"];
+      $requestBody = ["manufacturer" =>  "Gutamaya", "name" => "Imperial Eagle", "size" => "small"];
       $req =  $req->withParsedBody($requestBody);
       $this->app->getContainer()['request'] = $req;
 
-      // actually run the request through the app.
+      // run request through the app
       $response = $this->app->run(true);
       // assert expected status code and body
       $this->assertSame(200, $response->getStatusCode());
@@ -129,7 +130,7 @@ class ShipTest extends TestCase
     // test ship update failed due to invalid fields
     public function testUpdateShipFailed() {
       // expected result string
-      $resultString = '{"id":"1","manufacturer":"C.S. Lewis","name":"49","size":"writer"}';
+      $resultString = '{"id":"1","manufacturer":"Faulcon-Delacy","name":"Sidewinder","size":"small"}';
 
       // mock the query class & fetchAll functions
       $query = $this->createMock('mockQuery');
@@ -137,55 +138,55 @@ class ShipTest extends TestCase
         ->willReturn(json_decode($resultString, true)
       );
       $this->db->method('query')
-            ->willReturn($query);
+        ->willReturn($query);
        $this->db->method('exec')
-          ->will($this->throwException(new PDOException()));
+        ->will($this->throwException(new PDOException()));
 
-      // mock the request environment.  (part of slim)
+      // mock request environment
       $env = Environment::mock([
           'REQUEST_METHOD' => 'PUT',
-          'REQUEST_URI'    => '/people/1',
+          'REQUEST_URI'    => '/ships/1',
           ]);
       $req = Request::createFromEnvironment($env);
-      $requestBody = ["manufacturer" =>  "C.S. Lewis", "name" => "49", "size" => "writer"];
+      $requestBody = ["manufacturer" =>  "Faulcon-Delacy", "name" => "Sidewinder", "size" => "small"];
       $req =  $req->withParsedBody($requestBody);
       $this->app->getContainer()['request'] = $req;
 
-      // actually run the request through the app.
+      // run the request through the app
       $response = $this->app->run(true);
       // assert expected status code and body
       $this->assertSame(400, $response->getStatusCode());
-      $this->assertSame('{"status":400,"messname":"Invalid data provided to update"}', (string)$response->getBody());
+      $this->assertSame('{"status":400,"message":"Update failed: Invalid or Insufficient data provided."}', (string)$response->getBody());
     }
 
-    // test ship update failed due to persn not found
+    // test ship update failed because ship was not found
     public function testUpdateShipNotFound() {
       // expected result string
-      $resultString = '{"id":"1","manufacturer":"C.S. Lewis","name":"49","size":"writer"}';
+      $resultString = '{"id":"1","manufacturer":"Faulcon-Delacy","name":"Sidewinder","size":"small"}';
 
       // mock the query class & fetchAll functions
       $query = $this->createMock('mockQuery');
       $query->method('fetch')->willReturn(false);
       $this->db->method('query')
-            ->willReturn($query);
-       $this->db->method('exec')
-          ->will($this->throwException(new PDOException()));
+        ->willReturn($query);
+      $this->db->method('exec')
+        ->will($this->throwException(new PDOException()));
 
-      // mock the request environment.  (part of slim)
+      // mock request environment
       $env = Environment::mock([
           'REQUEST_METHOD' => 'PUT',
-          'REQUEST_URI'    => '/people/1',
+          'REQUEST_URI'    => '/ships/1',
           ]);
       $req = Request::createFromEnvironment($env);
       $requestBody = ["manufacturer" =>  "C.S. Lewis", "name" => "49", "size" => "writer"];
       $req =  $req->withParsedBody($requestBody);
       $this->app->getContainer()['request'] = $req;
 
-      // actually run the request through the app.
+      // run request through the app
       $response = $this->app->run(true);
       // assert expected status code and body
       $this->assertSame(404, $response->getStatusCode());
-      $this->assertSame('{"status":404,"messname":"not found"}', (string)$response->getBody());
+      $this->assertSame('{"status":404,"message":"not found"}', (string)$response->getBody());
 
     }
 
@@ -195,12 +196,12 @@ class ShipTest extends TestCase
       $this->db->method('exec')->willReturn(true);
       $env = Environment::mock([
           'REQUEST_METHOD' => 'DELETE',
-          'REQUEST_URI'    => '/people/1',
+          'REQUEST_URI'    => '/ships/1',
           ]);
       $req = Request::createFromEnvironment($env);
       $this->app->getContainer()['request'] = $req;
 
-      // actually run the request through the app.
+      // run request through the app.
       $response = $this->app->run(true);
       // assert expected status code and body
       $this->assertSame(200, $response->getStatusCode());
@@ -212,15 +213,15 @@ class ShipTest extends TestCase
       $this->db->method('exec')->willReturn(false);
       $env = Environment::mock([
           'REQUEST_METHOD' => 'DELETE',
-          'REQUEST_URI'    => '/people/1',
+          'REQUEST_URI'    => '/ships/1',
           ]);
       $req = Request::createFromEnvironment($env);
       $this->app->getContainer()['request'] = $req;
 
-      // actually run the request through the app.
+      // run request through the app
       $response = $this->app->run(true);
       // assert expected status code and body
       $this->assertSame(404, $response->getStatusCode());
-      $this->assertSame('{"status":404,"messname":"not found"}', (string)$response->getBody());
+      $this->assertSame('{"status":404,"message":"not found"}', (string)$response->getBody());
     }
 }
